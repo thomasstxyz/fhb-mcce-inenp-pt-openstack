f15224f35a403a1833afb830a235597187b47162
thomasstxyz <thomas@thomasst.xyz>
2022-05-27 02:15:23 +0200

    create K8s cluster w/ Ansible

---
+++ ansible/README.md
@@ -0,0 +1,8 @@
+Install required Ansible Roles:
+
+    ansible-galaxy role install -r roles/requirements.yml
+
+Run the Ansible Playbook:
+
+    ansible-playbook -i inventory playbook.yml
+
--- terraform/security_groups.tf
+++ terraform/security_groups.tf
@@ -14,3 +14,45 @@ resource "openstack_networking_secgroup_rule_v2" "secgroup_rule_ingress_ssh" {
   remote_ip_prefix  = "0.0.0.0/0"
   security_group_id = "${openstack_networking_secgroup_v2.secgroup_1.id}"
 }
+
+resource "openstack_networking_secgroup_rule_v2" "secgroup_rule_ingress_kubeapi" {
+  direction         = "ingress"
+  ethertype         = "IPv4"
+  protocol          = "tcp"
+  port_range_min    = 6443
+  port_range_max    = 6443
+  remote_ip_prefix  = "10.0.0.0/24"
+  security_group_id = "${openstack_networking_secgroup_v2.secgroup_1.id}"
+}
+
+resource "openstack_networking_secgroup_rule_v2" "secgroup_rule_ingress_nodeportrange" {
+  direction         = "ingress"
+  ethertype         = "IPv4"
+  protocol          = "tcp"
+  port_range_min    = 30000
+  port_range_max    = 32767
+  remote_ip_prefix  = "0.0.0.0/0"
+  security_group_id = "${openstack_networking_secgroup_v2.secgroup_1.id}"
+}
+
+# TODO: replace with ports needed for Kubernetes cluster networking
+resource "openstack_networking_secgroup_rule_v2" "secgroup_rule_ingress_allports_tcp" {
+  direction         = "ingress"
+  ethertype         = "IPv4"
+  protocol          = "tcp"
+  port_range_min    = 1
+  port_range_max    = 65535
+  remote_ip_prefix  = "0.0.0.0/0"
+  security_group_id = "${openstack_networking_secgroup_v2.secgroup_1.id}"
+}
+
+# TODO: replace with ports needed for Kubernetes cluster networking
+resource "openstack_networking_secgroup_rule_v2" "secgroup_rule_ingress_allports_udp" {
+  direction         = "ingress"
+  ethertype         = "IPv4"
+  protocol          = "udp"
+  port_range_min    = 1
+  port_range_max    = 65535
+  remote_ip_prefix  = "0.0.0.0/0"
+  security_group_id = "${openstack_networking_secgroup_v2.secgroup_1.id}"
+}
